aeb0f2c02ab2ddd6bc9d9694fe670904a5c40600
jasvitha2305 <jasvitha2305@gmail.com>
2022-01-19 20:12:16 +0530

    new

---
+++ Week10Assignment/src/test/java/com/assi/Week10AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
